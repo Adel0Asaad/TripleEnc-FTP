519e159dbc66d351d739b577a31f5fb4cd180581
Adel Asaad <69366493+Adel0Asaad@users.noreply.github.com>
2023-01-12 01:26:47 +0200

    made FTP server and made the GUI upload successfully
    Co-Authored-By: Adel Asaad <69366493+Adel0Asaad@users.noreply.github.com>
    Co-Authored-By: MadonnaBassem99 <70482229+MadonnaBassem99@users.noreply.github.com>

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from codecs import ascii_encode
 import tkinter as tk
+from tkinter import messagebox
 import threading
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tokenize import String
@@ -13,10 +14,10 @@ import ftplib
 
 def upload():
     # FTP server credentials 
-    FTP_HOST = "127.0.0.1" 
-    FTP_PORT = 6060 
-    FTP_USER = "username" 
-    FTP_PASS = "P@ssw0rd" 
+    FTP_HOST = "ftp.byethost14.com" 
+    FTP_PORT = 21 
+    FTP_USER = "b14_33366562" 
+    FTP_PASS = "7odazftt" 
     # connect to the FTP server 
     ftp = ftplib.FTP() 
     ftp.connect(FTP_HOST,FTP_PORT) 
@@ -24,18 +25,21 @@ def upload():
     # force UTF-8 encoding 
     ftp.encoding = "utf-8" 
     # local file name you want to upload 
-    filename = "sometextfile.txt" 
+    filename = "FileToUpload.txt" 
+    ftp.cwd("/htdocs")
     with open(filename, "rb") as file: 
         # use FTP's STOR command to upload the file 
         ftp.storbinary(f"STOR {filename}", file) 
     # quit and close the connection 
+    print('Should be uploaded')
+
     ftp.quit()
 
 def download():
-    FTP_HOST = "127.0.0.1" 
-    FTP_PORT = 6060 
-    FTP_USER = "username" 
-    FTP_PASS = "P@ssw0rd" 
+    FTP_HOST = "ftp.byethost14.com" 
+    FTP_PORT = 21 
+    FTP_USER = "b14_33366562" 
+    FTP_PASS = "7odazftt" 
     # connect to the FTP server 
     ftp = ftplib.FTP() 
     ftp.connect(FTP_HOST,FTP_PORT) 
@@ -43,15 +47,17 @@ def download():
     # force UTF-8 encoding 
     ftp.encoding = "utf-8" 
     # the name of file you want to download from the FTP server 
-    filename = "anothertextfile.txt" 
+    filename = "FileToUpload.txt" 
     with open(filename, "wb") as file: 
         # use FTP's RETR command to download the file 
         ftp.retrbinary(f"RETR {filename}", file.write) 
     # quit and close the connection 
     ftp.quit()
 
-def textApp():
+def print_hello():
+    messagebox.showinfo("Message", "hi")
 
+def textApp():
 
     window = tk.Tk()
     window.title("Team 28 Client")
@@ -60,11 +66,11 @@ def textApp():
 
     txt_edit = tk.Text(window, bg="#242424", fg="#FFFFFF", insertbackground="#CCCCCC")
     fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-    btn_open = tk.Button(fr_buttons, text="Connect")
+    btn_connect = tk.Button(fr_buttons, text="Connect", command= upload)
     btn_save = tk.Button(fr_buttons, text="Save As...")
     btn_add_text = tk.Button(fr_buttons, text="Insert Hello")
 
-    btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    btn_connect.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
     btn_save.grid(row=1, column=0, sticky="ew", padx=5)
     btn_add_text.grid(row=2, column=0, sticky="ew", padx=5, pady=10)
 
