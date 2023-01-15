3b36e2e8d43fa6087bbc68753d6b29b5947ed454
Mohamed Adel <malotfe99@gmail.com>
2023-01-15 16:47:59 +0200

    made the owner select the file to upload

--- Owner/owner_GUI.py
+++ Owner/owner_GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 from PIL import ImageTk
 import threading
 import ftplib
@@ -163,7 +164,8 @@ def upload():
     ftp.login(FTP_USER,FTP_PASS) 
     # force UTF-8 encoding 
     ftp.encoding = "utf-8" 
-    # local file name you want to upload 
+    # local file name you want to upload
+    file = filedialog.askopenfilename(initialdir='/', title="select file") 
     file1 = "encrypted_data.bin" 
     file2 = "encrypted_key.bin"
     with open(file1, "rb") as file: 
